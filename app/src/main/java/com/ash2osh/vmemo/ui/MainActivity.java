5f20faf350d66fa7d985d8e50b30d7f39f3fb922
ash2osh <orochimaru.ash@gmail.com>
2018-06-23 20:55:19 +0200

    initial

---
+++ app/src/main/java/com/ash2osh/vmemo/ui/MainActivity.java
@@ -0,0 +1,15 @@
+package com.ash2osh.vmemo.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.ash2osh.vmemo.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
